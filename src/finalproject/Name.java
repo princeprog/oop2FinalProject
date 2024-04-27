243e02fded4eda70a436b1492c68dbcdf1728108
Suenlie <Suenlie@DESKTOP-QLJROL8>
2024-04-27 16:34:58 +0800

    first commit

---
+++ src/finalproject/Name.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalproject;
+
+/**
+ *
+ * @author Suenlie
+ */
+public class Name {
+    String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    
+}
